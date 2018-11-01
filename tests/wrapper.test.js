69a011c5c016f40beab92f609220e17f2aa3f1be
Peter Peterson <ppeterson@splunk.com>
2018-10-31 22:14:51 -0700

    BREAKING CHANGE: update to node10

--- tests/wrapper.test.js
+++ tests/wrapper.test.js
@@ -1,159 +1,133 @@
 /*
  * Copyright (c) 2011 Yahoo! Inc. All rights reserved.
  */
-/*jslint nomen: true */
-var Y = require('yuitest'),
-    A = Y.Assert,
-    wrap = require('../index').wrap;
+const { assert } = require('chai');
+const { wrap } = require('../index');
 
-Y.TestRunner.add(new Y.TestCase({
+const assertableCallback = done => (num, str, str2) => {
+  assert.equal(1, num);
+  assert.equal('foo', str);
+  assert.equal('hello', str2);
+  done();
+};
 
-    name : 'wrapper test case',
+const getLogger = message => ({
+  warn: (str) => {
+    assert.isTrue(str.indexOf(message) === 0, `Log message did not match. Expected "${str}" to match "${message}"`);
+  },
+});
 
-    getLogger: function (message) {
-        return {
-            warn: function (str) {
-                A.isTrue(str.indexOf(message) === 0, 'Log message did not match. Expected "' + str + '" to match "' + message + '"');
-            }
-        };
-    },
-
-    "should fail if the user is stupid": function () {
-        try {
-            wrap();
-            A.fail('should have thrown an error');
-        } catch (err) {
-            A.areEqual('Callback not defined', err.message);
-        }
-        try {
-            wrap("foo");
-            A.fail('should have thrown an error');
-        } catch (err) {
-            A.areEqual('Callback not defined', err.message);
-        }
-    },
-    "should wrap a callback object": function () {
-        var cb = function (num, str, str2) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-                A.areEqual("hello", str2);
-            },
-            callback = wrap(cb);
-
-        this.wait(function () {
-            callback(1, "foo", "hello");
-        }, 1);
-    },
-    "should accept a custom logger": function () {
-        var cb = function (num, str, str2) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-                A.areEqual("hello", str2);
-            },
-            callback = wrap({
-                logger: this.getLogger('Call took ')
-            }, cb);
-
-        this.wait(function () {
-            callback(1, "foo", "hello");
-        }, 1);
-    },
-    "should accept a tag": function () {
-        var cb = function (num, str, str2) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-                A.areEqual("hello", str2);
-            },
-            callback = wrap({
-                tag: 'foo',
-                logger: this.getLogger('[foo] Call took ')
-            }, cb);
-
-        this.wait(function () {
-            callback(1, "foo", "hello");
-        }, 1);
-    },
-    "should accept a tag array": function () {
-        var cb = function (num, str, str2) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-                A.areEqual("hello", str2);
-            },
-            callback = wrap({
-                tag: ['foo', 'bar'],
-                logger: this.getLogger('[foo bar] Call took ')
-            }, cb);
-
-        this.wait(function () {
-            callback(1, "foo", "hello");
-        }, 1);
-    },
-    "should accept a maxTimeWarning for determining call length": function () {
-        var cb = function (num, str, str2) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-                A.areEqual("hello", str2);
-            },
-            callback = wrap({
-                maxTimeWarning: 20,
-                tag: 'foo',
-                logger: this.getLogger('[foo] Call took longer than 0.02 seconds - ')
-            }, cb);
-
-        this.wait(function () {
-            callback(1, "foo", "hello");
-        }, 30);
-    },
-    "should accept a method name to determine what the callback was for": function () {
-        var cb = function (num, str, str2) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-                A.areEqual("hello", str2);
-            },
-            callback = wrap({
-                methodName: 'banana',
-                tag: 'foo',
-                logger: this.getLogger('[foo] Call (banana) took ')
-            }, cb);
-
-        this.wait(function () {
-            callback(1, "foo", "hello");
-        }, 1);
-    },
-    "should not warn if maxTimeWarning and call was fast": function () {
-        var cb = function (num, str, str2) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-                A.areEqual("hello", str2);
-            },
-            callback = wrap({
-                methodName: 'banana',
-                maxTimeWarning: 20,
-                tag: 'foo',
-                logger: {
-                    warn: function () {
-                        A.fail('should not call the logger');
-                    }
-                }
-            }, cb);
-
-        this.wait(function () {
-            callback(1, "foo", "hello");
-        }, 1);
-    },
-    "should accept useRelativeTime": function () {
-        var cb = function (num, str) {
-                A.areEqual(1, num);
-                A.areEqual("foo", str);
-            },
-            callback = wrap({
-                logger: this.getLogger('Call took '),
-                useRelativeTime: false
-            }, cb);
-
-        this.wait(function () {
-            callback(1, "foo");
-        }, 1);
+/* eslint-env mocha */
+/* eslint-disable func-names */
+describe('callback-timer', () => {
+  it('should fail if the user is stupid', () => {
+    try {
+      wrap();
+      assert.fail('should have thrown an error');
+    } catch (err) {
+      assert.equal('Callback not defined', err.message);
     }
+    try {
+      wrap('foo');
+      assert.fail('should have thrown an error');
+    } catch (err) {
+      assert.equal('Callback not defined', err.message);
+    }
+  });
 
-}));
+  it('should wrap a callback object', function (done) {
+    this.timeout(1);
+    const cb = assertableCallback(done);
+    const callback = wrap(cb);
+
+    callback(1, 'foo', 'hello');
+  });
+
+  it('should accept a custom logger', function (done) {
+    this.timeout(1);
+    const cb = assertableCallback(done);
+    const callback = wrap({
+      logger: getLogger('Call took '),
+    }, cb);
+
+    callback(1, 'foo', 'hello');
+  });
+
+  it('should accept a tag', function (done) {
+    this.timeout(1);
+    const cb = assertableCallback(done);
+    const callback = wrap({
+      tag: 'foo',
+      logger: getLogger('[foo] Call took '),
+    }, cb);
+
+    callback(1, 'foo', 'hello');
+  });
+
+  it('should accept a tag array', function (done) {
+    this.timeout(1);
+    const cb = assertableCallback(done);
+    const callback = wrap({
+      tag: ['foo', 'bar'],
+      logger: getLogger('[foo bar] Call took '),
+    }, cb);
+
+    callback(1, 'foo', 'hello');
+  });
+
+  it('should accept a maxTimeWarning for determining call length', function (done) {
+    this.timeout(30);
+    const cb = assertableCallback(done);
+    const callback = wrap({
+      maxTimeWarning: 20,
+      tag: 'foo',
+      logger: getLogger('[foo] Call took longer than 0.02 seconds - '),
+    }, cb);
+
+    callback(1, 'foo', 'hello');
+  });
+
+  it('should accept a method name to determine what the callback was for', function (done) {
+    this.timeout(1);
+    const cb = assertableCallback(done);
+    const callback = wrap({
+      methodName: 'banana',
+      tag: 'foo',
+      logger: getLogger('[foo] Call (banana) took '),
+    }, cb);
+
+    callback(1, 'foo', 'hello');
+  });
+
+  it('should not warn if maxTimeWarning and call was fast', function (done) {
+    this.timeout(1);
+    const cb = assertableCallback(done);
+    const callback = wrap({
+      methodName: 'banana',
+      maxTimeWarning: 20,
+      tag: 'foo',
+      logger: {
+        warn() {
+          assert.fail('should not call the logger');
+        },
+      },
+    }, cb);
+
+    callback(1, 'foo', 'hello');
+  });
+
+  it('should accept useRelativeTime', function (done) {
+    this.timeout(1);
+    const cb = (num, str) => {
+      assert.equal(1, num);
+      assert.equal('foo', str);
+      done();
+    };
+    const callback = wrap({
+      logger: getLogger('Call took '),
+      useRelativeTime: false,
+    }, cb);
+
+    callback(1, 'foo');
+  });
+});
